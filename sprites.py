8faaec598cca7c22af6745159823f79a293c64ee
nintanuki <34449029+nintanuki@users.noreply.github.com>
2026-04-16 12:55:24 -0400

    Remove unused code

--- sprites.py
+++ sprites.py
@@ -306,9 +306,6 @@ class Alien(pygame.sprite.Sprite):
         # Blue aliens zigzag
         self.blue_zigzag_direction = random.choice([-1,1]) # 1 for right, -1 for left
 
-        # Give green aliens twin lasers
-        self.is_twin = True if color == 'green' else False
-
         self.value = AlienSettings.POINTS.get(color, 0)
 
     def destroy(self):
